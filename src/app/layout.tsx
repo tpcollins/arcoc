1b5cc65511503dc90e4282cd3d772a6e6490faea
tpcollins <tpcollins>
2024-09-12 12:49:18 -0600

    trying to fix bootstrap js errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { ReactNode, useEffect } from 'react';
 import localFont from "next/font/local";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -17,32 +16,36 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   useEffect(() => {
-//     require('bootstrap/dist/js/bootstrap.bundle.min.js'); 
-//   }, []);
-
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   useEffect(() => {
-    // Dynamically import Bootstrap JS
-    import('bootstrap/dist/js/bootstrap.bundle.min.js');
+    require('bootstrap/dist/js/bootstrap.bundle.min.js'); 
   }, []);
 
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   useEffect(() => {
+//     // Dynamically import Bootstrap JS
+//     import('bootstrap/dist/js/bootstrap.bundle.min.js');
+//   }, []);
+
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </>
   );
 }
+
+
