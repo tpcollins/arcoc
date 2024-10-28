426cb5244a6ce32f0b34a878c0344867e8da378c
tpcollins <tpcollins>
2024-10-28 10:36:36 -0600

    adding test page to see if there is another issue 10-28

---
+++ src/app/testpage/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+const Verbose: React.FC = () => {
+
+    return(
+        <>
+            <h1>Test Page</h1>
+        </>
+    )
+};
+
+export default Verbose;
