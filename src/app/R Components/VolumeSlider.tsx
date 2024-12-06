8aab7f0b11d35fc824064c7e5c5fa7c97ff02416
tpcollins <tpcollins00@gmail.com>
2024-12-05 20:47:12 -0700

    added some styling. for some reason adding in marks causes issues with the translator. going to fix that later 12-5

--- src/app/R Components/VolumeSlider.tsx
+++ src/app/R Components/VolumeSlider.tsx
@@ -1,47 +1,3 @@
-// import React, { useState } from 'react';
-// import Slider from 'rc-slider';
-// import "rc-slider/assets/index.css";
-
-// const VolumeSlider: React.FC = () => {
-//   const [volume, setVolume] = useState(100);
-
-//   const handleSliderChange = (value: number | number[]) => {
-//     // If the value is an array (multi-slider), use the first element, otherwise use the number directly
-//     const newValue = Array.isArray(value) ? value[0] : value;
-//     setVolume(newValue);
-//   }; 
-  
-
-//   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const newValue = Number(e.target.value);
-//     if (newValue >= 0 && newValue <= 200) {
-//       setVolume(newValue);
-//     }
-//   };
-
-//   return (
-//     <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%' }}>
-//       <Slider
-//         value={volume}
-//         onChange={handleSliderChange}
-//         min={0}
-//         max={200}
-//         style={{ flexGrow: 1, marginRight: '10px' }} // Flex-grow makes it stretch
-//       />
-//       <input
-//         type="number"
-//         value={volume}
-//         onChange={handleInputChange}
-//         min="0"
-//         max="100"
-//         style={{ width: '60px' }}
-//       />
-//     </div>
-//   );
-// };
-
-// export default VolumeSlider;
-
 import React, { useState } from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
@@ -74,6 +30,14 @@ const VolumeSlider: React.FC<VolumeSliderProps> = ({ onVolumeChange }) => {
         onChange={handleSliderChange}
         min={0}
         max={200}
+        // marks={{
+        //   0: '0',
+        //   50: '|',
+        //   100: '100',
+        //   150: '|',
+        //   200: '200',
+        // }}
+        step={10}
         style={{ flexGrow: 1, marginRight: '10px' }}
       />
       <input
@@ -82,7 +46,14 @@ const VolumeSlider: React.FC<VolumeSliderProps> = ({ onVolumeChange }) => {
         onChange={handleInputChange}
         min="0"
         max="200"
-        style={{ width: '60px' }}
+        style={{
+          width: '60px',
+          border: '1px solid #4caf50',
+          borderRadius: '5px',
+          padding: '5px',
+          color: '#4caf50',
+          background: '#fff',
+        }}
       />
     </div>
   );
